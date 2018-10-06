8e9233c896e5052093d98a21bfdd76d3b43a75c8
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2018-10-06 10:26:41 +0300

    Add edit link

--- src/containers/Page.tsx
+++ src/containers/Page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { withRouteData } from 'react-static'
 import convert from 'htmr'
 import { PageData } from '../types'
-import { PageMeta } from "../components/PageMeta";
+import { PageMeta } from '../components/PageMeta'
 
 interface Props {
   page: PageData
@@ -12,5 +12,13 @@ export default withRouteData(({ page }: Props) => (
   <div>
     <PageMeta title={page.title} />
     {convert(page.content)}
+    <hr />
+    <a
+      href={`https://github.com/ilkkahanninen/hulinapedia/edit/master/content/pages/${
+        page.slug
+      }`}
+    >
+      Muokkaa sivua GitHubissa
+    </a>
   </div>
 ))
