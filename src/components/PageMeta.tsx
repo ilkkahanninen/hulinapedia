81b87d76d581699fc7d03b9cf4942acc230ea069
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2018-10-08 16:07:55 +0300

    Add default image for OpenGraph

--- src/components/PageMeta.tsx
+++ src/components/PageMeta.tsx
@@ -10,33 +10,20 @@ interface Props {
 export const PageMeta: React.SFC<Props> = ({ title, image, description }) => {
   const fullTitle = `Hulinapedia${title ? ` – ${title}` : ''}`
   const descr = description || 'Hulinan virallinen sala-arkisto'
+  const imageSrc = `https://hulinapedia.netlify.com/images/${image ||
+    'hulina-opengraph.png'}`
 
   return (
     <Head>
       <title>{fullTitle}</title>
       <meta name="description" content={descr} />
-
       <meta property="og:site_name" content={title} />
       <meta property="og:description" content={descr} />
-      {image && (
-        <meta
-          property="og:image"
-          content={`https://hulinapedia.netlify.com/images/${image}`}
-        />
-      )}
+      <meta property="og:image" content={imageSrc} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://hulinapedia.netlify.com" />
       <meta property="og:title" content={title} />
       <meta property="og:locale" content="fi_FI" />
-
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={descr} />
-      {image && (
-        <meta
-          property="twitter:image"
-          content={`https://hulinapedia.netlify.com/images/${image}`}
-        />
-      )}
     </Head>
   )
 }
